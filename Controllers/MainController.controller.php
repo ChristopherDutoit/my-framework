a8d59e2628f76220c9d889f6feff6a57e1409a2c
christopher <christopher.dutoit2002@gmail.com>
2024-01-23 13:45:33 +0100

    Toolbox

--- Controllers/MainController.controller.php
+++ Controllers/MainController.controller.php
@@ -1,10 +1,20 @@
 <?php
 require_once 'Services/Render.php';
+require_once 'Models/MainManager/MainManager.php';
+require_once 'Services/Toolbox.php';
 class MainController extends Render{
+
+    private $mainManager;
+
+    public function __construct(){
+        parent::__construct(Render::class);
+        $this->mainManager = new MainManager;
+    }
     public function accueil(){
         $data_page = [
             "page_description" => "Description de la page d'accueil",
             "page_title" => "Titre de la page d'accueil",
+            "page_css" => ["accueil.css"],
             "view" => "Views/Accueil.view.php",
             "template" => "Views/Layouts/base.php"
         ];
@@ -12,9 +22,13 @@ class MainController extends Render{
     }
 
     public function page1(){
+        $datas = $this->mainManager->getDatas();
+        Toolbox::addAlert('test', Toolbox::COULEUR_VERTE);
         $data_page = [
             "page_description" => "Description de la page 1",
             "page_title" => "Page 1",
+            "page_css" => ["pages.css"],
+            "datas" => $datas,
             "view" => "Views/Page1.view.php",
             "template" => "Views/Layouts/base.php"
         ];
@@ -36,7 +50,8 @@ class MainController extends Render{
             "page_description" => "Description de la page 3",
             "page_title" => "Page 3",
             "view" => "Views/Page3.view.php",
-            "template" => "Views/Layouts/base.php"
+            "template" => "Views/Layouts/base.php",
+            "page_javascript" => "page3.js"
         ];
         $this->render($data_page);
     }
--- Views/Layouts/base.php
+++ Views/Layouts/base.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?=$page_description; ?>">
     <title><?=$page_title;?></title>
+    <link href="<?= URL ?>public/style/main.css" rel="stylesheet"/>
+    <?php if(!empty($page_css)) : ?>
+        <?php foreach($page_css as $p) : ?>
+            <link href="<?= URL ?>public/style/<?= $p ?>" rel="stylesheet" />
+        <?php endforeach; ?>
+    <?php endif; ?>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
@@ -21,28 +27,22 @@
 
 </head>
 <body>
+    <?php require_once('Views/Layouts/header.php') ; ?>
     <?php require_once('Views/Layouts/menu.php');?>
+    <?php require_once('Services/Toolbox.php');?>
+<?php
+try{
+    Toolbox::displayAlerts();
+} catch (Exception $e){
+    throw new \RuntimeException($e->getMessage());
+}
+?>
     <?= $page_content; ?>
-
+    <?php require_once('Views/Layouts/footer.php') ; ?>
 </body>
-<script>
-    document.addEventListener('DOMContentLoaded', function(){
-    let dropdownButton = document.getElementById('dropdownButton');
-    let dropdownMenu = document.getElementById('dropdownMenu');
-    let menuButtton = document.getElementById('menuButton');
-    let mobileMenu = document.getElementById('mobileMenu');
+<script src="<?= URL ?>/public/Javascript/navbar.js"> </script>
 
-    if(dropdownButton && dropdownMenu) {
-        dropdownButton.addEventListener('click', function() {
-            dropdownMenu.classList.toggle('hidden');
-        })
-    }
-
-    if(menuButton && mobileMenu) {
-        menuButton.addEventListener('click', function() {
-            mobileMenu.classList.toggle('hidden');
-        })
-    }
-})
-</script>
+<?php if(!empty($page_javascript)) : ?>
+    <script src="<?= URL?>public/Javascript/<?= $page_javascript ?>"></script>
+    <?php endif; ?>
 </html>
