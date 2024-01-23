4d3ea9a6eebd35825be85d90722fef3e8d6a0b2e
christopher <christopher.dutoit2002@gmail.com>
2024-01-23 08:35:04 +0100

    first commit

---
+++ Views/Layouts/menu.php
@@ -0,0 +1,15 @@
+<nav class="bg-gray-800">
+    <div class="container mx-auto px-6 py-3 lg:flex lg:justify-between lg:items-center">
+        <button id="menuButton" class="text-white focus:outline-none lg:hidden" type="button">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor"
+                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-liejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+        </buton>
+
+        <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="mobileMenu">
+            <ul class="lg:flex items-center justify-between text-base text-wite pt-4 lg:pt-0">
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+$page_description = "Description de la page d'accueil";
+$page_title = "Titre de la page d'accueil";
+$page_content="<h1 class='text-2xl text-blue-600 text-center'>Hello World</h1>";
+require_once("Views/Layouts/base.php");
